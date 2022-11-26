741a387672aab366d452cf20a3eca84b3ed3f714
Debanjan Basak <92038706+debanjanbasak23@users.noreply.github.com>
2022-11-26 15:36:41 +0530

    Add files via upload

---
+++ chess - displaying data/chess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chess
+//
+//  Created by admin on 04/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
