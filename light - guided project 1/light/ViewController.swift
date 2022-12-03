46ac4c4695433d78034bfa7ff1ec12ec4649f87e
Debanjan Basak <92038706+debanjanbasak23@users.noreply.github.com>
2022-12-03 12:27:57 +0530

    Add files via upload

---
+++ light - guided project 1/light/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  light
+//
+//  Created by ADMIN on 03/11/22.
+//
+
+import UIKit
+     
+class ViewController: UIViewController {
+
+    @IBOutlet var lightButton: UIButton!
+    
+    var lightOn = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+}
